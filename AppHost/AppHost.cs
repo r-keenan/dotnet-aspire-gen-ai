e37e2bdede2fdc8348ca14f8e3320c746c644da2
r-keenan <ross.keenan@protonmail.com>
2025-08-06 15:00:08 -0500

    added postgres and created databases for each microservice

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,6 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Basket>("basket");
-builder.AddProject<Projects.Catalog>("catalog");
+var postgres = builder
+        .AddPostgres("postgres")
+        .WithPgAdmin()
+        .WithDataVolume()
+        .WithLifetime(ContainerLifetime.Persistent);
+
+var basketDb = postgres.AddDatabase("basketDb");
+var catalogDb = postgres.AddDatabase("catalogDb");
+
+var basket = builder
+        .AddProject<Projects.Basket>("basket")
+        .WithReference(basketDb)
+        .WaitFor(basketDb);
+var catalog = builder
+        .AddProject<Projects.Catalog>("catalog")
+        .WithReference(catalogDb)
+        .WaitFor(catalogDb);
 
 builder.Build().Run();
